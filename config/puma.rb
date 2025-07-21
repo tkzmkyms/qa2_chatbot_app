4c708bdff173a9134c6c2a56706a537f45d0a2af
tkzmkyms <tkzmkyms1002@gmail.com>
2025-07-21 09:19:38 +0900

    デプロイ出来ない為の修正

--- config/puma.rb
+++ config/puma.rb
@@ -24,8 +24,8 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+threads 3, 3
+
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
